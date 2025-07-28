209e029e962d6890afedccfcb546ba7718dd74ef
Alexandermodestov <aleksandrmodestov@gmail.com>
2025-07-28 23:30:43 +0100

    fixed prompt

--- src/lib/orchestrator.ts
+++ src/lib/orchestrator.ts
@@ -141,15 +141,6 @@ function createPrompt(childAge: number, sleepHistory: SleepSession[], childGende
   }
 
   const formattedEntries = sleepHistory.map(formatSession).join('\n')
-  // Use local time for current context
-  const currentDate = new Date().toLocaleString('en-CA', { 
-    year: 'numeric', 
-    month: '2-digit', 
-    day: '2-digit', 
-    hour: '2-digit', 
-    minute: '2-digit',
-    hour12: false 
-  }).replace(', ', 'T')
   const babyProfile = { name: childName, gender: childGender }
   const babyAge = `${childAge} months`
   
